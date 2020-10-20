7bef948321996160602b698ae65d3c1105e43136
Hjalte Juel Skovrup <hjsk@itu.dk>
2020-10-20 17:43:06 +0200

    changes to UI added useless box

---
+++ agiprog/Data/MeetingID.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace agiprog.Data
+{
+    public class MeetingID
+    {
+        public String meetingId { get; set; }
+    }
+}
