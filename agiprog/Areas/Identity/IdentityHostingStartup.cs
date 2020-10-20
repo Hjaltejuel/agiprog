d0d64c706e3a2c9907b9248b78b9d96b3389bd4a
Hjalte Juel Skovrup <hjsk@itu.dk>
2020-10-20 16:38:22 +0200

    first commit

---
+++ agiprog/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using agiprog.Areas.Identity.Data;
+using agiprog.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(agiprog.Areas.Identity.IdentityHostingStartup))]
+namespace agiprog.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContextPool<agiprogContext>(options =>
+                    options.UseMySql(
+                        context.Configuration.GetConnectionString("agiprogContextConnection")));
+
+                services.AddDefaultIdentity<agiprogUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<agiprogContext>();
+            });
+        }
+    }
+}
