f36420218607f95a050fba5a3967ee90359d5f4f
hjalteSkovrup <hjalte@festinafinance.com>
2020-11-24 13:15:24 +0100

    MaxPoolSIze

--- agiprog/Startup.cs
+++ agiprog/Startup.cs
@@ -49,6 +49,7 @@ namespace agiprog
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+         
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -56,7 +57,9 @@ namespace agiprog
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                //app.UseExceptionHandler("/Error");
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
